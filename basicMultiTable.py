4f5002ef89326acd26d27ae7a5b75895c911d83d
HunterSD23 <148793726+HunterSD23@users.noreply.github.com>
2024-09-27 14:51:35 +0000

    started writing the variables

---
+++ basicMultiTable.py
@@ -0,0 +1,24 @@
+"""
+Program: Basic Multiplication Table
+Author: Hunter Dettmer
+
+1. Ask the user for a number
+2. Print out the name of the multiplication table:
+    "Multiplication Table for <number>:"
+3. for Loop print out all the multiplication facts (1 to 12) for the users number
+"""
+
+# Create your Code
+
+"""
+Multiplication Table Generator with Custom Range, and Multi-Table Option
+
+1. Ask the user if they want to generate multiple tables
+2. Create a Funtion to do steps 2 & 3 above (3 variables: number, start=1, end=12)
+3. Create a control structure to handle the user answer from step 1
+    What do we do if yes
+    What do we do if no
+    (only one will nest a control structure...?)
+"""
+
+rangedNumber = int(input("Please enter a number from 1 to 12: "))
